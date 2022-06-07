13c613e587f4ec09aa95afdd82f5b2358a759683
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-07 23:32:05 +0300

    Continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (planDo = "roller-skate"){
+    return `This Saturday, I want to ${planDo}!`};
+    saturdayFun()
+    saturdayFun("bathe my dog");
+
+    function mondayWork (Mon = "go to the office"){
+        return `This Monday, I will ${Mon}.`;
+    }
+mondayWork()
+mondayWork("be early");
+function wrapAdjective(testing = "*"){
+    return function (mypar = "special"){
+        return `You are ${testing}${mypar}${testing}!`;
+    }
+}
+wrapAdjective()('a diligent youth');
+wrapAdjective("a dedicated programmer");
